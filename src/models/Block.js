34fde46ce0307053fd6f2e46a1073a041ce9dd4f
yangxue12 <2325449412@qq.com>
2023-06-09 20:42:49 +0800

    update lesson5

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,25 +1,69 @@
 import sha256 from 'crypto-js/sha256.js'
-
+import UTXOPool from './UTXOPool.js'
 export const DIFFICULTY = 2
 
 class Block {
-  // 1. 完成构造函数及其参数
-
-  constructor() {}
-
-  isValid() {}
-
-  setNonce(nonce) {}
-
-  // 根据交易变化更新区块 hash
-  _setHash() {}
-
-  // 汇总计算交易的 Hash 值
+    // 1. 完成构造函数及其参数
+    /* 构造函数需要包含
+    区块链
+    上一个区块的哈希值
+    高度
+    时间戳
+    */
+    constructor(blockchain, previousHash, height, timestamp,coinbaseBeneficiary) {
+    this.blockchain = blockchain
+    this.previousHash = previousHash
+    this.height = height
+    this.timestamp = timestamp //输出时间戳
+    this.hash = this.calculateHash()
+    this.nonce=0
+    this.utxoPool=new UTXOPool()
+    this.coinbaseBeneficiary=coinbaseBeneficiary
+    this.transactions=[]
+    }
+    calculateHash () {
+    return sha256(this.nonce+this.previousHash+this.blockchain+this.height+this.timestamp+DIFFICULTY).toString()
+    }
+    _setHash(){
+      this.hash=this.calculateHash()
+    }
+    
+    /* 验证当前区块是否有效 */
+    isValid () {
+    //是否满足难度值
+    if (this.calculateHash().substring(0,DIFFICULTY) == Array(DIFFICULTY+1).join("0")) {
+    return true
+    }
+    }
+    
+    setNonce(nonce) {
+    this.nonce++
+    return this.hash=sha256(this.nonce+nonce+this.previousHash+this.blockchain+this.height+this.timestamp+DIFFICULTY).toString()
+    }
+     // 汇总计算交易的 Hash 值
   /**
    * 默克尔树实现
    */
   combinedTransactionsHash() {
-
+    const transactions = this.transactions.map(transaction => transaction.hash);
+    if (transactions.length === 0) {
+        return sha256('').toString();
+    } else if (transactions.length === 1) {
+        return transactions[0];
+    } else {
+        let nodes = transactions;
+        while (nodes.length > 1) {
+            const parents = [];
+            for (let i = 0; i < nodes.length; i += 2) {
+                const left = nodes[i]
+                const right = i + 1 === nodes.length ? left : nodes[i + 1];
+                const parent = sha256(left + right).toString();
+                parents.push(parent);
+            }
+            nodes = parents;
+        }
+        return nodes[0];
+    }
   }
 
   // 添加交易到区块
@@ -27,7 +71,23 @@ class Block {
    * 
    * 需包含 UTXOPool 的更新与 hash 的更新
    */
-  addTransaction() {}
+  addTransaction (transaction) {
+    if (!transaction.isValidTransaction()) return
+
+    // 处理交易
+    this.utxoPool.handleTransaction(transaction)
+
+    // 添加交易到 transactions 数组
+    this.transactions.push(transaction)
+
+    // 计算交易 hash
+    transaction._setHash()
+
+    // 更新区块的 hash 和 merkleTree
+    this.combinedTransactionsHash()
+    this._setHash()
+
+  }
 
 }
 
--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -2,18 +2,35 @@ import sha256 from 'crypto-js/sha256.js'
 
 
 class Transaction {
-  constructor() {
-    
+  constructor(fromPublicKey,toPublicKey,amount) {
+    this.toPublicKey=toPublicKey
+    this.fromPublicKey=fromPublicKey
+    this.amount=amount
+    this.timestamp=new Date().getTime()
+    this.hash=this._calculateHash()
   }
 
   // 更新交易 hash
   _setHash() {
-
+    this.hash=this._calculateHash()
   }
 
   // 计算交易 hash 的摘要函数
   _calculateHash() {
-   
+   return sha256(this.fromPublicKey+this.toPublicKey+this.amount+this.timestamp).toString()
+  }
+  isValidTransaction () {
+    // 验证交易金额是否非负
+    if (this.amount <= 0) {
+      return false;
+    }
+
+    // 验证交易哈希是否正确
+    if (this.hash !== this._calculateHash()) {
+      return false;
+    }
+
+    return true;
   }
 }
 
